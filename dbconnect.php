a0fda7b24b105b10557d0e51866add0201473959
jackie-code <paradiseprofessional88@gmail.com>
2019-09-15 05:49:27 +0800

    redid the dbconnect

--- dbconnect.php
+++ dbconnect.php
@@ -1,14 +1,38 @@
+
 <?php
-// set the default connection values.
-$host = "localhost:3306"; // use 127.0.0.1:8889 if you're not using port 3306
-$dbname = "ecommerce31";
-$username = "root";
-$password = "root";
-// Try to connect to the DB server and select a specific DB:
-try {
- // PDO database functions go here: connect, query, etc.
- $db = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-} catch (PDOException $e) {
- echo $e->getMessage();
- exit(); // Optional, but probably a good idea if things have failed
+//default for localhost values
+$host = "localhost";
+$port = "3306";
+$user = "root";
+$pw = "root";
+$dbname = "ecommerce";
+
+// for PBCS server
+if (gethostname() == "server.pbcs.us")
+{
+$host = "localhost";
+$port = "3306";
+$user = "jmartin";
+$password = "codeschool1";
+$database = "jmartin_ecommerce";
 }
+
+$connection_info = 
+"mysql" 
+.": host=" . $host
+.":" . $port
+. "; dbname=" . $dbname
+;
+ 
+$db = new PDO($connection_info, $user, $pw);
+
+
+
+
+
+
+
+
+
+
+
